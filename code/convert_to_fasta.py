87fa036571a2bba68b0faa26fbb5476d856d34ea
meghalD <meghal.dani.er@gmail.com>
2018-04-11 22:48:24 +0530

    Add files via upload

---
+++ code/convert_to_fasta.py
@@ -0,0 +1,30 @@
+import sys
+
+#File input
+file_text = input("enter the name to text file: ")
+fileInput = open(file_text, "r")
+
+#File output
+file_fasta = input("Enter the name of fasta file: ")
+fileOutput = open(file_fasta, "w")
+
+#Seq count
+count = 1 ;
+
+#Loop through each line in the input file
+print("Converting to FASTA...")
+for strLine in fileInput:
+
+    #Strip the endline character from each input line
+    strLine = strLine.rstrip("\n")
+
+    #Output the header
+    fileOutput.write(">" + str(count) + "\n")
+    fileOutput.write(strLine + "\n")
+
+    count = count + 1
+print ("Done.")
+
+#Close the input and output file
+fileInput.close()
+fileOutput.close()
